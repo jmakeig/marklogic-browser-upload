d07467cdbf8ffbf78947e0c3216c9acdc9e7cf02
Justin Makeig <jmpublic+github@makeig.com>
2016-01-09 15:04:18 -0800

    Starts to wire up actions

--- browser/src/index.js
+++ browser/src/index.js
@@ -1,4 +1,5 @@
+import 'number-to-locale-string';
 import {createStore} from 'redux';
-
-createStore();
-console.log('Say, "Hi!"');
+import {DATABASE_STATS_REFRESH} from './actions'
+createStore(() => 4);
+console.log('Say, "%s"', DATABASE_STATS_REFRESH);
