607133ea45a11bad98d5584e2f0322422474e909
Justin Makeig <jmpublic+github@makeig.com>
2016-01-09 14:33:05 -0800

    Creates webpack build
    This was a long and winding road. (Why is this so complicated?) My end
    result is slightly different than most that I’ve seen.
    
    1. I build to same directory where the HTML and CSS live, so I don’t
    have to build those as well. I’m not too keen on webpack managing my
    CSS (and bootstrapping it from JavaScript).
    2. There’s nothing about React in there (yet)
    3. I haven’t yet enabled hot reloading or the dev server.

---
+++ browser/src/index.js
@@ -0,0 +1,4 @@
+import {createStore} from 'redux';
+
+createStore();
+console.log('Say, "Hi!"');
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+var webpack = require('webpack');
+var path = require('path');
+
+const PATHS = {
+  app: path.join(__dirname, 'browser'),
+  build: path.join(__dirname, 'browser') //path.join(__dirname, 'build')
+};
+
+console.log(PATHS.app);
+
+module.exports = {
+  context: PATHS.app,
+  entry: {
+    //html: './index.html',
+    js: './src/index.js',
+    vendor: ['redux'] // List vendor libraries that will show up in vendor.bundle.js
+  },
+  output: {
+    path: PATHS.build,
+    filename: 'bundle.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.html$/,
+        loader: 'file?name=[name].[ext]'
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loaders: [
+          'babel-loader'
+        ]
+      },
+      {
+        test: /\.css$/,
+        loaders: ['style-loader', 'css-loader'],
+        include: PATHS.app
+      }
+    ],
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin(/* chunkName= */"vendor", /* filename= */"vendor.bundle.js")
+  ]
+}
