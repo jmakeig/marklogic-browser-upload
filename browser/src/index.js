3e0f8ee13087c629e817457538b437c4619366fc
Justin Makeig <jmpublic+github@makeig.com>
2016-01-09 15:06:55 -0800

    Installs Number.prototype.toLocaleString

--- browser/src/index.js
+++ browser/src/index.js
@@ -2,4 +2,5 @@ import 'number-to-locale-string';
 import {createStore} from 'redux';
 import {DATABASE_STATS_REFRESH} from './actions'
 createStore(() => 4);
-console.log('Say, "%s"', DATABASE_STATS_REFRESH);
+const num = 41234567890;
+console.log('Say, "%s" and %s', DATABASE_STATS_REFRESH, num.toLocaleString('en-us'));
