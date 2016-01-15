52d2e9c243bdf32069672eddc9230f1896c6caa8
Justin Makeig <jmpublic+github@makeig.com>
2016-01-14 17:04:06 -0800

    Better defaults

--- browser/src/reducers/index.js
+++ browser/src/reducers/index.js
@@ -12,39 +12,7 @@ const initialState = {
 	databaseID: "16204519326364673683",
 	isFetchingDatabaseStats: false,
 	locale: navigator.language,
-	databaseStats: {
-	  "id": "16204519326364673683",
-	  "name": "Documents",
-	  "documentsCount": 0,
-	  "propertiesCount": 0,
-	  "collections": [
-	    {
-	      "name": "(none)",
-	      "count": 0,
-	      "isNone": true,
-				isUpdated: false // Use this to indicate something that the UI should highlight as changed
-	    }
-	  ],
-	  "batches": [],
-	  "documentFormats": [
-	    {
-	      "format": "json",
-	      "count": 0
-	    },
-	    {
-	      "format": "xml",
-	      "count": 0
-	    },
-	    {
-	      "format": "binary",
-	      "count": 0
-	    },
-	    {
-	      "format": "text",
-	      "count": 0
-	    }
-	  ]
-	},
+	databaseStats: null,
 	files: {
 		uploadProgress: null,
 		fileList: null
