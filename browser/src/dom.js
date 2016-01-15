c5739910fe2c082b4f71ebe79833143e33b41fdb
Justin Makeig <jmpublic+github@makeig.com>
2016-01-14 22:30:32 -0800

    Adds locale parameter

--- browser/src/dom.js
+++ browser/src/dom.js
@@ -1,12 +1,14 @@
 'use strict'
 
-function _el(localname, classList, attrs, contents) {
+const DEFAULT_LOCALE = 'en-US';
+
+function _el(localname, classList, attrs, contents, locale) {
   var elem = document.createElement(localname || 'div');
   if('string' === typeof contents) {
     elem.textContent = contents;
   } else if('number' === typeof contents) {
-    console.warn('Using hard-coded locale.');
-    elem.textContent = contents.toLocaleString('en-us'); // FIXME: Get locale from model/store
+    // console.warn('Using hard-coded locale.');
+    elem.textContent = contents.toLocaleString(locale || DEFAULT_LOCALE); // FIXME: Get locale from model/store
   }
   else if(contents instanceof HTMLElement) {
     elem.appendChild(contents);
@@ -30,12 +32,13 @@ function _el(localname, classList, attrs, contents) {
   }
   return elem;
 }
-export function div   (t, c, a) {return _el('div',    c, a, t);}
-export function h1    (t, c, a) {return _el('h1',     c, a, t);}
-export function h2    (t, c, a) {return _el('h2',     c, a, t);}
-export function h3    (t, c, a) {return _el('h3',     c, a, t);}
-export function tr    (t, c, a) {return _el('tr',     c, a, t);}
-export function td    (t, c, a) {return _el('td',     c, a, t);}
-export function button(t, c, a) {return _el('button', c, a, t);}
-export function span  (t, c, a) {return _el('span',   c, a, t);}
-export function p     (t, c, a) {return _el('p',      c, a, t);}
+
+export function div   (t, c, a, l) {return _el('div',    c, a, t, l);}
+export function h1    (t, c, a, l) {return _el('h1',     c, a, t, l);}
+export function h2    (t, c, a, l) {return _el('h2',     c, a, t, l);}
+export function h3    (t, c, a, l) {return _el('h3',     c, a, t, l);}
+export function tr    (t, c, a, l) {return _el('tr',     c, a, t, l);}
+export function td    (t, c, a, l) {return _el('td',     c, a, t, l);}
+export function button(t, c, a, l) {return _el('button', c, a, t, l);}
+export function span  (t, c, a, l) {return _el('span',   c, a, t, l);}
+export function p     (t, c, a, l) {return _el('p',      c, a, t, l);}
